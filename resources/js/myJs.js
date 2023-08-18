e9627af5d2d93b5d58e3016341fe313ba58aa2ec
star23sharp <injoon990721@gmail.com>
2023-08-18 17:29:24 +0900

    moadl

---
+++ resources/js/myJs.js
@@ -0,0 +1,11 @@
+// script 태그 내에 작성하던 javascript 코드 작성
+
+function f_rgbcolor() {
+    let red = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+
+    return "rgb("+red+", "+blue+", "+green+")";
+}
+
+const v_phone = "01042547152"
